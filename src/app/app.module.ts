5469210c92e85acb841ce655115dc4a7e315f1ef
gklein18 <66079216+gklein18@users.noreply.github.com>
2026-07-02 22:43:26 -0500

    feat: Update app module to declare StatsComponent

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StatsComponent } from './components/stats/stats.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StatsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
